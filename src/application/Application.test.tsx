d87c3060f42ed2c784653dd9a60609af451e4d96
Siege Songsong <cjgsongsong@gmail.com>
2026-06-20 23:16:30 +0800

    test: Test internationalization

--- src/application/Application.test.tsx
+++ src/application/Application.test.tsx
@@ -1,8 +1,17 @@
 import { render } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import Application from "./Application";
+import internationalize from "./internationalization";
+
+vi.mock("./internationalization", { spy: true });
 
 describe("Application", () => {
+  it("should internationalize text", () => {
+    render(<Application />);
+
+    expect(internationalize).toHaveBeenCalledOnce();
+  });
+
   it("should render its main content", () => {
     const { getByTestId } = render(<Application />);
 
